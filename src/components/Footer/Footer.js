797875c1979e486ecfa1eff931a8d8614490b1f6
Piyush55dude <piyushagarwalvo@gmail.com>
2021-07-04 22:08:21 +0530

    Footer Updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
       <hr style={{ width: "90%", marginTop: 20 }} />
       <span className="name">
         Made by{" "}
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
-          Piyush Agarwal
+        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+          Roadside Coder
         </a>
       </span>
       <div className="iconContainer">
@@ -18,8 +18,8 @@ const Footer = () => {
         <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://piyushjsx.netlify.app/" target="__blank">
-          <i className="fas fa-link fa-2x"></i>
+        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+          <i className="fab fa-youtube fa-2x"></i>
         </a>
       </div>
     </div>
